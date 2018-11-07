ce5ea50a5a4d5b753bbd48003286ed2d3cfa5395
Bellardia <ian@jumbleberry.com>
2018-11-07 13:18:54 -0500

    Fixes for “base” recipe

--- base/metadata.rb
+++ base/metadata.rb
@@ -6,6 +6,8 @@ description "Configures the basebox for all Jumbleberry Applications"
 version "0.1.0"
 chef_version ">= 12.1" if respond_to?(:chef_version)
 
+depends "apt"
+depends "configure"
 depends "consul"
 depends "consul-template"
 depends "trusted_certificate"
--- base/recipes/consul-template.rb
+++ base/recipes/consul-template.rb
@@ -3,5 +3,10 @@ include_recipe "consul-template"
 edit_resource(:template, "/etc/systemd/system/consul-template.service") do
   source "consul-template.service.erb"
   cookbook "base"
-  notifies :stop, "service[consul-template]", :immediate
+  notifies :run, "execute[systemctl-daemon-reload]", :immediately
+  notifies :stop, "service[consul-template]", :delayed
+end
+
+edit_resource(:service, "consul-template") do
+  action %i[enable stop]
 end
