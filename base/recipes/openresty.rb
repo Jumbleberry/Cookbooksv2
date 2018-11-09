ca6fa0de741ba2a7c4aeed4cddf287f7467a1206
Bellardia <ian@jumbleberry.com>
2018-11-09 02:08:24 -0500

    remove duplicate resources

--- base/recipes/openresty.rb
+++ base/recipes/openresty.rb
@@ -2,7 +2,7 @@ include_recipe "openresty::default"
 
 delete_resource(:template, "#{node["openresty"]["dir"]}/conf.d/http_realip.conf")
 
-directory "#{node["openresty"]["dir"]}/ssl" do
+edit_resource(:directory, "#{node["openresty"]["dir"]}/ssl") do
   owner node["openresty"]["user"]
   group node["openresty"]["user"]
   mode "0760"
