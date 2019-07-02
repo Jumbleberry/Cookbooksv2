5f9c07133218cfacfb6ada6f45bd601bea9f79ac
nicolaipetrov <nicolai@lyricalsecurity.com>
2019-07-02 11:17:48 -0400

    NP: openresty recipes updated - OpenSSL path added  (ubuntu.18-ready)

--- base/recipes/openresty.rb
+++ base/recipes/openresty.rb
@@ -1,5 +1,11 @@
+# Custom OpenSSL
+# Openssl directory path 
+if node['lsb']['release'].to_f > 16
+  node.default['openresty']['source']['default_configure_flags'] |= [ "--with-openssl=/var/chef/cache/openssl-1.0.2p" ]
+end
 include_recipe "openresty::default"
 
+#
 delete_resource(:template, "#{node["openresty"]["dir"]}/conf.d/http_realip.conf")
 
 edit_resource(:directory, "#{node["openresty"]["dir"]}/ssl") do
