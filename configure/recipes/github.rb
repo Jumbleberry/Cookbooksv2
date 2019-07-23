05e1dc99542cf4a7ad69b07ea99e96d943fd9fa4
Bellardia <ian@jumbleberry.com>
2019-07-23 01:49:52 -0400

    Vault & SSH settings

---
+++ configure/recipes/github.rb
@@ -0,0 +1,37 @@
+directory "#{node["etc"]["passwd"][node[:user]]["dir"]}/.ssh" do
+  owner node[:user]
+  group node[:user]
+  recursive true
+end
+cookbook_file "#{node["etc"]["passwd"][node[:user]]["dir"]}/.ssh/config" do
+  source "config"
+  owner node[:user]
+  group node[:user]
+  mode "0600"
+  action :create
+end
+
+if node.attribute?(:ec2)
+  template "#{node["etc"]["passwd"][node[:user]]["dir"]}/.ssh/jumbleberry-github.tpl" do
+    source "jumbleberry-github.tpl"
+    mode "0644"
+    only_if { (keys = Vault.logical.read("secret/data/#{node["environment"]}/keys")) && !keys.data[:data][:deploybot].nil? }
+    notifies :create, "consul_template_config[jumbleberry-github]", :immediately
+    owner node[:user]
+    group node[:user]
+  end
+  consul_template_config "jumbleberry-github" do
+    templates [{
+      source: "#{node["etc"]["passwd"][node[:user]]["dir"]}/.ssh/jumbleberry-github.tpl",
+      destination: "#{node["etc"]["passwd"][node[:user]]["dir"]}/.ssh/jumbleberry-github",
+    }]
+    action :nothing
+    notifies :enable, "service[consul-template]", :immediate
+    notifies :start, "service[consul-template]", :immediate
+    notifies :reload, "service[consul-template]", :immediate
+  end
+end
+
+ssh_known_hosts_entry "github" do
+  host "github.com"
+end
