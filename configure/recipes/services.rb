f02c3cd70d750194c1e455b76883e33e028aaafe
nicolaipetrov <50152701+nicolaipetrov@users.noreply.github.com>
2019-07-24 11:34:21 -0400

    NP: using Systemd for services.
    Updated services.rb, using Systemd for all services.

--- configure/recipes/services.rb
+++ configure/recipes/services.rb
@@ -1,20 +1,25 @@
 edit_resource(:service, "dnsmasq") do
+  provider Chef::Provider::Service::Systemd
   supports :status => true, :restart => true, :reload => true
 end
 
 edit_resource(:service, "php#{node["php"]["version"]}-fpm") do
+  provider Chef::Provider::Service::Systemd
   supports :status => true, :restart => true, :reload => true
 end
 
 edit_resource(:service, "consul") do
+  provider Chef::Provider::Service::Systemd
   supports :status => true, :restart => true, :reload => true
 end
 
 edit_resource(:service, "redis@6379") do
+  provider Chef::Provider::Service::Systemd
   supports :status => true, :restart => true, :reload => true
 end
 
 edit_resource(:service, "consul-template") do
+  provider Chef::Provider::Service::Systemd
   supports :status => true, :restart => true, :reload => true
 end
 
@@ -24,9 +29,11 @@ edit_resource(:service, "nginx") do
 end
 
 edit_resource(:service, "gearman-job-server") do
+  provider Chef::Provider::Service::Systemd
   supports :status => true, :restart => true, :reload => true
 end
 
 edit_resource(:service, "gearman-manager") do
+  provider Chef::Provider::Service::Systemd
   supports :status => true, :restart => true, :reload => true
 end
