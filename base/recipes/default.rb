e0fc6102c6350f2a74f060ff753dc876f20bc5e2
nicolaipetrov <nicolai@lyricalsecurity.com>
2019-08-08 10:06:40 -0400

    NP: set home dir-s for user:WWW-data

--- base/recipes/default.rb
+++ base/recipes/default.rb
@@ -8,7 +8,7 @@ end
 
 include_recipe cookbook_name + "::packages"
 include_recipe cookbook_name + "::kernel"
-include_recipe cookbook_name + "::filesystem"
+#include_recipe cookbook_name + "::filesystem"
 include_recipe cookbook_name + "::redis"
 include_recipe cookbook_name + "::consul"
 include_recipe cookbook_name + "::vault"
