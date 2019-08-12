7c9bbcbaf01cb4103022aa2bf9fc4add0c193e68
Bellardia <ian@jumbleberry.com>
2019-08-11 20:12:20 -0400

    Remove unused code

--- configure/recipes/openresty.rb
+++ configure/recipes/openresty.rb
@@ -1,9 +1,3 @@
-if node.attribute?(:ec2)
-  edit_resource(:directory, "/var/www") do
-    mode 00770
-  end
-end
-
 include_recipe "openresty::commons_conf"
 
 edit_resource(:template, "nginx.conf") do
