aa9fd3d9deb5e2858ad1017f08fe808ba85ff9cc
Bellardia <ian@jumbleberry.com>
2019-09-23 23:47:55 -0400

    Move default nginx route out of configure

--- configure/recipes/openresty.rb
+++ configure/recipes/openresty.rb
@@ -11,9 +11,3 @@ edit_resource(:template, "nginx.conf") do
   source "nginx.conf.erb"
   cookbook "configure"
 end
-
-openresty_site "default" do
-  template "default.conf.erb"
-  timing :immediately
-  action :enable
-end
