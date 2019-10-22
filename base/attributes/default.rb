c8e34c16135f81105f0738ce21af2df5ccdb91b2
Bellardia <ian@jumbleberry.com>
2019-10-22 17:37:39 -0400

    Set default password

--- base/attributes/default.rb
+++ base/attributes/default.rb
@@ -105,3 +105,5 @@ default["gearman"]["manager"]["revision"] = "ffc828dac2547aff76cb4962bb3fcc4f454
 default["phalcon"]["install_script"] = "https://packagecloud.io/install/repositories/phalcon/stable/script.deb.sh"
 default["phalcon"]["version"] = "3.4.1-1+php#{php_version}"
 default["phalcon"]["devtools"] = "https://github.com/phalcon/phalcon-devtools.git"
+
+default["mysql"]["root_password"] = "root"
