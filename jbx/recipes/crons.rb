e01b832d91e510e73b3db2974430386f603ae1ef
Bellardia <ian@jumbleberry.com>
2020-02-25 20:14:34 -0500

    Add cron configuration

---
+++ jbx/recipes/crons.rb
@@ -0,0 +1,9 @@
+node["jbx"]["crons"].each do |definition|
+  cron definition[:name] || definition[:command] do
+    command definition[:command]
+    hour definition[:hour] || "*"
+    minute definition[:minute] || "0"
+    user node[:user]
+    action definition[:action] || :create
+  end
+end
