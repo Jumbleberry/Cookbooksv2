1357360ea183b64bec273ada54a2915e5aff9366
Bellardia <ian@jumbleberry.com>
2020-03-08 20:05:46 -0400

    check for key existance

--- jbx/recipes/crons.rb
+++ jbx/recipes/crons.rb
@@ -1,9 +1,11 @@
-node["jbx"]["crons"].each do |definition|
-  cron definition[:name] || definition[:command] do
-    command definition[:command]
-    hour definition[:hour] || "*"
-    minute definition[:minute] || "0"
-    user node[:user]
-    action definition[:action] || :create
+if node["jbx"].key?("crons") then
+  node["jbx"]["crons"].each do |definition|
+    cron definition[:name] || definition[:command] do
+      command definition[:command]
+      hour definition[:hour] || "*"
+      minute definition[:minute] || "0"
+      user node[:user]
+      action definition[:action] || :create
+    end
   end
 end
