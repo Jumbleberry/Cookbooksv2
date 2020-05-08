42d4ad17ef623c508ec4b13d42fd160655bb36b1
Bellardia <ian@jumbleberry.com>
2020-05-08 16:57:37 -0400

    Also use linux_generic

--- configure/recipes/common.rb
+++ configure/recipes/common.rb
@@ -2,6 +2,7 @@ include_recipe cookbook_name + "::user"
 include_recipe cookbook_name + "::ipaddress"
 include_recipe "opsworks_stack_state_sync"
 include_recipe "timezone_iii"
+include_recipe "timezone_iii::linux_generic"
 include_recipe "ntp"
 
 unless node.attribute?(:ec2)
