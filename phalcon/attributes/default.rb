fbb881ecc10da24a134a65f34a29eb988602c313
Bellardia <ian@jumbleberry.com>
2020-06-13 01:00:59 -0400

    PHP 7.3

--- phalcon/attributes/default.rb
+++
@@ -1,3 +0,0 @@
-default["phalcon"]["install_script"] = "https://packagecloud.io/install/repositories/phalcon/stable/script.deb.sh"
-default["phalcon"]["version"] = "3.4.1-1+php7.1"
-default["phalcon"]["devtools"] = "https://github.com/phalcon/phalcon-devtools.git"
