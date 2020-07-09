d0f37d4d27b5fb8db20275cd3fe014002ccee19e
Bellardia <ian@jumbleberry.com>
2020-07-09 13:37:36 -0400

    Make root user tied to a native password

--- configure/recipes/mysql.rb
+++ configure/recipes/mysql.rb
@@ -11,17 +11,23 @@ unless node.attribute?(:ec2)
     mode "0644"
   end
 
+  query = "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY \'#{node["mysql"]["root_password"]}\'; FLUSH PRIVILEGES;"
+  execute "set_root-password" do
+    command "echo \"#{query}\" | mysql -uroot"
+    only_if "echo 'show databases' | mysql -uroot mysql;"
+  end
+
   # Create jbx user
   query = "GRANT ALL ON *.* TO \'jbx\'@\'%\' IDENTIFIED BY \'#{node["mysql"]["root_password"]}\'"
   execute "create_jbx_user" do
-    command "echo \"#{query}\" | mysql -uroot"
-    only_if "echo 'show databases'  | mysql -uroot -#{node["mysql"]["root_password"]} mysql;"
+    command "echo \"#{query}\" | mysql -uroot -p#{node["mysql"]["root_password"]}"
+    only_if "echo 'show databases'  | mysql -uroot -p#{node["mysql"]["root_password"]} mysql;"
   end
 
   # Create 'root'@'%'
   query = "GRANT ALL ON *.* TO \'root\'@\'%\' IDENTIFIED BY \'#{node["mysql"]["root_password"]}\'"
   execute "create_root_user" do
-    command "echo \"#{query}\" | mysql -uroot"
-    only_if "echo 'show databases'  | mysql -uroot -#{node["mysql"]["root_password"]} mysql;"
+    command "echo \"#{query}\" | mysql -uroot -p#{node["mysql"]["root_password"]}"
+    only_if "echo 'show databases'  | mysql -uroot -p#{node["mysql"]["root_password"]} mysql;"
   end
 end
