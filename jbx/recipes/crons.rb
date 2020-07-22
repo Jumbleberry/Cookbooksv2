b37a29156e9423f6f68bcb5b40ffdf755d0cc4eb
Bellardia <ian@jumbleberry.com>
2020-07-22 10:41:51 -0400

    Support weekday and month

--- jbx/recipes/crons.rb
+++ jbx/recipes/crons.rb
@@ -2,6 +2,8 @@ if node["jbx"].key?("crons") then
   node["jbx"]["crons"].each do |definition|
     cron definition[:name] || definition[:command] do
       command definition[:command]
+      month definition[:month] || "*"
+      weekday definition[:weekday] || "*"
       hour definition[:hour] || "*"
       minute definition[:minute] || "0"
       user node[:user]
