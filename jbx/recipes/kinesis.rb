1da396fc526e666d7525ccd44c76aa820d362eb0
ahosking <alexander.hosking@jumbleberry.com>
2020-10-05 12:28:35 -0400

    Add Kinesis init script
    This uses the chef systemd unit module to create the jbx-kinesis service.
    The service will run the configurations specified by the props file.
    
    It is a simple service that should attempt to stream logs at all hours
    and should be enabled by default
    
    Resolves: DEV-190

--- jbx/recipes/kinesis.rb
+++ jbx/recipes/kinesis.rb
@@ -1,8 +1,21 @@
 # For setting up and running the java to support Kinesis Streams
-service "kinesis" do
-    supports :status => true, :restart => true
-    start_command "/usr/bin/java -cp \"/vagrant/www/jbx/application/modules/consumer/jars/*\" software.amazon.kinesis.multilang.MultiLangDaemon --properties-file /vagrant/www/jbx/application/modules/consumer/config/config.properties >> /var/log/jbx-kinesis.log"
-    restart_command "kill $(ps aux | grep java|grep kinesis | awk '{print $2}') && mv /var/log/jbx-kinesis.log /var/log/jbx-kinesis.log.old && /usr/bin/java -cp \"/vagrant/www/jbx/application/modules/consumer/jars/*\" software.amazon.kinesis.multilang.MultiLangDaemon --properties-file /vagrant/www/jbx/application/modules/consumer/config/config.properties >> /var/log/jbx-kinesis.log"
-    status_command "ps aux|grep java|grep kinesis"
-    action [ :enable, :start ]
+systemd_unit "jbx-kinesis.service" do
+    content(
+        {
+            Unit: { 
+                Description: 'Enables the JBX java processes for Kinesis data streaming',
+            },
+            Service: {
+                Type: 'simple',
+                ExecStart: '/usr/bin/java -cp "/var/www/jbx/application/modules/consumer/jars/*" software.amazon.kinesis.multilang.MultiLangDaemon --properties-file /var/www/jbx/application/modules/consumer/config/config.properties',
+                User: node[:user],
+                Restart: 'on-failure',
+                RestartSec: 10,
+            },
+            Install: {
+                WantedBy: 'multi-user.target',
+            }
+        }
+    )
+    action [:create, :enable]
 end
