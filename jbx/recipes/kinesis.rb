fd8443d6b2cac2c294a80a0a8949295abea0ff7d
Bellardia <ian@jumbleberry.com>
2020-10-15 00:39:10 -0400

    Abstract service to enable multiple consumer services

--- jbx/recipes/kinesis.rb
+++ jbx/recipes/kinesis.rb
@@ -1,21 +1,25 @@
 # For setting up and running the java to support Kinesis Streams
-systemd_unit "jbx-kinesis.service" do
-    content(
+if node["jbx"].attribute?(:consumers)
+  node["jbx"]["consumers"].each do |service, config|
+    systemd_unit service + ".service" do
+      content(
         {
-            Unit: { 
-                Description: 'Enables the JBX java processes for Kinesis data streaming',
-            },
-            Service: {
-                Type: 'simple',
-                ExecStart: '/usr/bin/java -cp "/var/www/jbx/application/modules/consumer/jars/*" software.amazon.kinesis.multilang.MultiLangDaemon --properties-file /var/www/jbx/application/modules/consumer/config/config.properties',
-                User: node[:user],
-                Restart: 'on-failure',
-                RestartSec: 10,
-            },
-            Install: {
-                WantedBy: 'multi-user.target',
-            }
+          Unit: {
+            Description: "KCL Service for " + service,
+          },
+          Service: {
+            Type: "simple",
+            ExecStart: "/usr/bin/java -cp \"#{node["jbx"]["path"]}/application/modules/consumer/jars/*\" software.amazon.kinesis.multilang.MultiLangDaemon --properties-file #{node["jbx"]["path"]}/application/modules/consumer/config/#{config}",
+            User: node[:user],
+            Restart: "on-failure",
+            RestartSec: 10,
+          },
+          Install: {
+            WantedBy: "multi-user.target",
+          },
         }
-    )
-    action [:create, :enable]
+      )
+      action [:create] + (node["configure"]["services"][service] || %i{stop disable})
+    end
+  end
 end
