b04efd2a6b618539406a1affb29fc6195c8a8a56
Bellardia <ian@jumbleberry.com>
2021-01-07 10:44:55 -0500

    Fix bug where all delayed actions were suppressed

--- base/recipes/default.rb
+++ base/recipes/default.rb
@@ -28,3 +28,29 @@ if node["environment"] == "dev"
 end
 
 include_recipe "security"
+
+ruby_block "clearing delayed notifications for disabled services" do
+  block do
+    services = node[:configure][:services] || {}
+
+    # Check all delayed notifications to ensure a service we care about isn't leaked by ending in an unexpected state
+    run_context.delayed_notification_collection.each do |from, notification_array|
+      notification_array.each do |notification|
+        resource_type = notification.resource.declared_type.to_s
+        resource_name = notification.resource.name
+
+        # Treat restart/reload as a subset of start
+        action = { "restart" => "start", "reload" => "start" }[notification.action.to_s] || notification.action.to_s
+
+        # See what actions are allowed
+        allowed_actions = (node["recipes"].include?("configure::services") ? services[resource_name] : []) || []
+
+        # One of the services we managing is trying to do
+        if resource_type.include?("service") && services.keys.include?(resource_name) && !allowed_actions.include?(action)
+          Chef::Log.warn("Overriding delayed action #{action} on #{resource_type}[#{resource_name}] to prevent service leak")
+          notification.action = :nothing
+        end
+      end
+    end
+  end
+end
