9e3b31791b360937279242a82f21b49a4e5a944a
Bellardia <ian@jumbleberry.com>
2021-01-07 00:12:19 -0500

    Run funcs async to allow multiple builds in parallel

--- jbx/recipes/ci.rb
+++ jbx/recipes/ci.rb
@@ -2,9 +2,10 @@ if node["environment"] != "prod"
   random_id = rand(36 ** 16).to_s(36)
   execute "phpunit" do
     command <<-EOH
-      #{node["jbx"]["path"]}/command github:check && \
-      #{node["jbx"]["path"]}/phpunit --log-junit /tmp/#{random_id}.xml > /tmp/#{random_id}.txt && \
-      #{node["jbx"]["path"]}/command github:check --junit /tmp/#{random_id}.xml --phpunit /tmp/#{random_id}.txt
+      #{node["jbx"]["path"]}/command github:check & (\
+        #{node["jbx"]["path"]}/phpunit --log-junit /tmp/#{random_id}.xml > /tmp/#{random_id}.txt; \
+        #{node["jbx"]["path"]}/command github:check --junit /tmp/#{random_id}.xml --phpunit /tmp/#{random_id}.txt \
+      ) &
     EOH
     cwd node["jbx"]["path"]
     action :run
