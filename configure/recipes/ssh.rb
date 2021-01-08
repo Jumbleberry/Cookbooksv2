4196845e4a7b6b0f34c20d3d7c07bdd187daaa7f
Bellardia <ian@jumbleberry.com>
2021-01-08 16:02:30 -0500

    Enable ssh login via password

--- configure/recipes/ssh.rb
+++ configure/recipes/ssh.rb
@@ -1,8 +1,15 @@
 unless node.attribute?(:ec2)
-  replace_or_add "sshd_config" do
+  replace_or_add "sshd_config_root_login" do
     path "/etc/ssh/sshd_config"
     pattern "PermitRootLogin*"
     line "PermitRootLogin yes"
+    notifies :restart, "service[sshd.service]"
+  end
+
+  replace_or_add "sshd_config_password_login" do
+    path "/etc/ssh/sshd_config"
+    pattern "PasswordAuthentication*"
+    line "PasswordAuthentication yes"
     notifies :restart, "service[sshd.service]"
   end
 
