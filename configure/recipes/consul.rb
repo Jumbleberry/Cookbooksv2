d235ed4316c2a0f318a62da32f26d89e79a8978c
Bellardia <ian@jumbleberry.com>
2021-02-14 01:23:11 -0500

    Consul & Datadog fixes

--- configure/recipes/consul.rb
+++ configure/recipes/consul.rb
@@ -1,1 +1,11 @@
-include_recipe "consul::default"
+consul_config node["consul"]["service_name"] do |r|
+  node["consul"]["config"].each_pair { |k, v| r.send(k, v) }
+end
+
+# If we reload consul to quickly after it starts, it crashes
+# As a result we'll only trigger a reload if it was already running
+execute "consul-reload-check" do
+  command "true"
+  only_if "systemctl is-active --quiet " + node["consul"]["service_name"]
+  notifies :reload, "service[consul.service]", :delayed
+end
