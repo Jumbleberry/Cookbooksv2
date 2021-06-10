da45c73804bf2bcf25507287e6a2b891caabe6f0
Bellardia <ian@jumbleberry.com>
2021-06-10 14:42:39 -0400

    Remove securetty settings which have been moved in 20.04

--- security/recipes/default.rb
+++ security/recipes/default.rb
@@ -5,7 +5,6 @@ when "ubuntu", "amazon", "centos"
   include_recipe "security::linux-icmp-redirect"
   #   include_recipe 'security::unix-umask-unsafe'
   include_recipe "security::generic-tcp-timestamp"
-  include_recipe "security::unix-anonymous-root-tty-logins"
   include_recipe "security::unix-anonymous-root-ssh-logins"
   include_recipe "security::unix-user-home-dir-mode"
 else
