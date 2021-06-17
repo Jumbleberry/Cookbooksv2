3422d24b9b2422f62285166928265406a43b9a7c
Bellardia <ian@jumbleberry.com>
2021-06-17 18:56:23 -0400

    Add mysql package deps for 18.04

--- configure/attributes/default.rb
+++ configure/attributes/default.rb
@@ -1,8 +1,14 @@
 cookbook_name = "configure"
 
 default[cookbook_name]["plugin_path"] = "/etc/chef/ohai_plugins"
-default[cookbook_name]["packages"] = ["git", "make", "curl", "unzip", "uuid", "redis-tools", "libpcre3-dev", "tzdata", "default-jre", "gcc"] +
-                                     (node["lsb"]["release"].to_f >= 18 ? ["libncurses5", "libpython2-stdlib", "libpython2.7-minimal", "libpython2.7-stdlib", "libtinfo5", "python-is-python2", "python2", "python2-minimal", "python2.7", "python2.7-minimal"] : [])
+default[cookbook_name]["packages"] = ["git", "make", "curl", "unzip", "uuid", "redis-tools", "libpcre3-dev", "tzdata", "default-jre", "gcc"]
+
+if (node["lsb"]["release"].to_i >= 20)
+  default[cookbook_name]["packages"] += ["libncurses5", "libpython2-stdlib", "libpython2.7-minimal", "libpython2.7-stdlib", "libtinfo5", "python-is-python2", "python2", "python2-minimal", "python2.7", "python2.7-minimal"]
+elsif (node["lsb"]["release"].to_i >= 18)
+  default[cookbook_name]["packages"] += ["libpython-stdlib", "libpython2.7-minimal", "libpython2.7-stdlib", "python", "python-minimal", "python2.7", "python2.7-minimal"]
+end
+
 default[cookbook_name]["update"] = false
 default[cookbook_name]["upgrade"] = false
 
