beed7f5736239f4aef5a3cbbed0f623847dba607
Bellardia <ian@jumbleberry.com>
2021-06-20 17:55:19 -0400

    Fix systemd-resolved configuration for 18.04

--- dnsmasq/recipes/default.rb
+++ dnsmasq/recipes/default.rb
@@ -6,7 +6,7 @@ if platform?("ubuntu") && node["platform_version"].to_i >= 18
 
   file "Fix systemd-resolved conflict" do
     path "/etc/systemd/resolved.conf.d/dnsmasq.conf"
-    content node["platform_version"].to_i >= 20 ? "[Resolve]\nDNSStubListener=no" : "[Resolve]\nnameserver 127.0.0.1"
+    content node["platform_version"].to_i >= 20 ? "[Resolve]\nDNSStubListener=no" : "[Resolve]\nDNS=127.0.0.1"
     notifies :restart, "service[systemd-resolved]", :immediately
   end
 
