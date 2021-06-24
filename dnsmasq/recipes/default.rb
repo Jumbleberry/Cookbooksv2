814ec38034df7efe7ac346a7dce4a12600a68894
Bellardia <ian@jumbleberry.com>
2021-06-24 15:25:48 -0400

    Fix resolve.conf symlink to be more sensible

--- dnsmasq/recipes/default.rb
+++ dnsmasq/recipes/default.rb
@@ -1,12 +1,21 @@
 package "dnsmasq"
 user "dnsmasq"
 
-if platform?("ubuntu") && node["platform_version"].to_i >= 18
+if platform?("ubuntu") && node["lsb"]["release"].to_i >= 18
   directory "/etc/systemd/resolved.conf.d"
 
   file "Fix systemd-resolved conflict" do
     path "/etc/systemd/resolved.conf.d/dnsmasq.conf"
     content node["platform_version"].to_i >= 20 ? "[Resolve]\nDNSStubListener=no" : "[Resolve]\nDNS=127.0.0.1"
+    notifies :restart, "service[systemd-resolved]", :immediately
+  end
+
+  # Fix symlink on resolve.conf
+  # https://askubuntu.com/a/1001295
+  link "/etc/resolv.conf" do
+    to "/run/systemd/resolve/resolv.conf"
+    action :create
+    only_if { node["lsb"]["release"].to_i < 20 }
     notifies :restart, "service[systemd-resolved]", :immediately
   end
 
