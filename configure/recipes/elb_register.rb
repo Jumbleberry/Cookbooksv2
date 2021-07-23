5c29614e3bd65afd9f7528df93ebfef5f09bc4da
Bellardia <ian@jumbleberry.com>
2021-07-23 14:49:17 -0400

    Fix typo in attribute check

--- configure/recipes/elb_register.rb
+++ configure/recipes/elb_register.rb
@@ -6,6 +6,6 @@ node["configure"]["elb"]["target_groups"].each do |target|
           --targets Id=#{node["ec2"]["instance_id"]} \
           --region #{node["ec2"]["placement_availability_zone"].sub!(/[a-zA-Z]*?$/, "")}
     EOH
-    only_if { node.attribute(:ec2) && node["ec2"]["instance_id"] }
+    only_if { node.attribute?(:ec2) && node["ec2"]["instance_id"] }
   end
 end
