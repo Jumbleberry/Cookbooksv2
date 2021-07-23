f0b5ba6493ec7746296629897acb171ac1e7668c
Bellardia <ian@jumbleberry.com>
2021-07-23 00:17:53 -0400

    Fix regex bug on replacement pattern

--- configure/recipes/ssh.rb
+++ configure/recipes/ssh.rb
@@ -1,14 +1,14 @@
 unless node.attribute?(:ec2)
   replace_or_add "sshd_config_root_login" do
     path "/etc/ssh/sshd_config"
-    pattern "PermitRootLogin*"
+    pattern "PermitRootLogin.*"
     line "PermitRootLogin yes"
     notifies :restart, "service[sshd.service]"
   end
 
   replace_or_add "sshd_config_password_login" do
     path "/etc/ssh/sshd_config"
-    pattern "PasswordAuthentication*"
+    pattern "PasswordAuthentication.*"
     line "PasswordAuthentication yes"
     notifies :restart, "service[sshd.service]"
   end
