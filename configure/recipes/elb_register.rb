28c132512ba3a42e818dbe7982d78597e4334b65
Bellardia <ian@jumbleberry.com>
2021-07-23 10:18:40 -0400

    Add commands for elb registration

---
+++ configure/recipes/elb_register.rb
@@ -0,0 +1,11 @@
+node["configure"]["elb"]["target_groups"].each do |target|
+  execute "add_to_elb #{target}" do
+    command <<-EOH
+      aws elbv2 register-targets \
+          --target-group-arn #{target} \
+          --targets Id=#{node["ec2"]["instance_id"]} \
+          --region #{node["ec2"]["placement_availability_zone"].sub!(/[a-zA-Z]*?$/, "")}
+    EOH
+    only_if { node.attribute(:ec2) && node["ec2"]["instance_id"] }
+  end
+end
