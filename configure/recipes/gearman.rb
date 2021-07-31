4d126061d8f12d184afd52dc89019d376e2535fa
Bellardia <ian@jumbleberry.com>
2021-07-30 23:06:45 -0400

    Improve gearman recipes

--- configure/recipes/gearman.rb
+++ configure/recipes/gearman.rb
@@ -1,9 +1,14 @@
+template "/etc/gearman-manager/environment" do
+  source "gearman-environment.erb"
+end
+
 edit_resource(:service, "gearman-job-server.service") do
   subscribes :restart, "template[/etc/default/gearman-job-server]", :delayed
 end
 
 edit_resource(:service, "gearman-manager.service") do
   subscribes :restart, "template[/etc/default/gearman-job-server]", :delayed
+  subscribes :restart, "template[/etc/gearman-manager/environment]", :delayed
 end
 
 template "/etc/default/gearman-job-server" do
