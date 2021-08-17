0d4604fa81765f1806c600f722babbc14459f9a5
Bellardia <ian@jumbleberry.com>
2021-08-17 15:17:53 -0400

    Stop seeding CI databases, Pretend to be CI

--- jbx/recipes/ci.rb
+++ jbx/recipes/ci.rb
@@ -1,11 +1,4 @@
 if node["environment"] != "prod"
-  execute "seed_dev_db" do
-    command "#{node["jbx"]["path"]}/command seed:fresh --load-dump --up --no-interaction"
-    environment ({ "ENV" => node[:environment] })
-    user node[:user]
-    action :run
-  end
-
   random_id = node["jbx"]["branch"].gsub(/[^0-9A-Za-z]/, "-") + "_" + rand(36 ** 8).to_s(36)
   execute "phpunit" do
     command <<-EOH
@@ -14,7 +7,12 @@ if node["environment"] != "prod"
         #{node["jbx"]["path"]}/phpunit --log-junit /tmp/#{random_id}.xml > /tmp/#{random_id}.txt; \
         #{node["jbx"]["path"]}/command github:check --shasum #{node["jbx"]["branch"]} --junit /tmp/#{random_id}.xml --phpunit /tmp/#{random_id}.txt; \
         #{node["jbx"]["path"]}/command seed:fresh --drop --no-interaction; \
-        DATADOG_API_KEY=#{node["datadog"]["api_key"]} DD_ENV=ci datadog-ci junit upload --service jbx unit-tests/junit-reports /tmp/#{random_id}.xml \
+        DATADOG_API_KEY=#{node["datadog"]["api_key"]} DD_ENV=ci \
+            GITHUB_ACTION=codebuild \
+            GITHUB_REPOSITORY=$(git config --get remote.origin.url | cut -d ":" -f2 | cut -f 1 -d '.') \
+            GITHUB_SHA=$(git rev-parse HEAD) \
+            GITHUB_HEAD_REF=$(git branch -a --contains HEAD 2>/dev/null | sed -n 2p | awk '{ printf $1 }' | cut -c16-) \
+            datadog-ci junit upload --service jbx unit-tests/junit-reports /tmp/#{random_id}.xml \
       ) &
     EOH
     environment ({ "ENV" => node[:environment] })
