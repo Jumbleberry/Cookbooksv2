fc84cc9f5f38916c008010f39491e57d6da9350b
Bellardia <ian@jumbleberry.com>
2022-04-23 09:44:08 -0400

    Add support for datadog profiler

--- base/recipes/datadog.rb
+++ base/recipes/datadog.rb
@@ -1,5 +1,19 @@
 include_recipe "datadog::dd-agent"
 
+cookbook_file "datadog-setup.php" do
+  path "/usr/local/bin/datadog-setup.php"
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+  notifies :run, "execute[datadog-profiler]", :immediately
+end
+
+execute "datadog-profiler" do
+  command "/usr/bin/php '/usr/local/bin/datadog-setup.php' '--enable-profiling' '--php-bin=php' '--php-bin=php#{node["php"]["version"]}' '--php-bin=php-fpm#{node["php"]["version"]}'"
+  action :nothing
+end
+
 edit_resource(:service, "datadog-agent") do
   action %i{disable stop}
 end
