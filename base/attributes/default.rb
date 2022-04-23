2d64fccde253adf8663debd8bb4039985248ea60
Bellardia <ian@jumbleberry.com>
2022-04-23 09:44:27 -0400

    Add Management for Datadog PHP Extensions

--- base/attributes/default.rb
+++ base/attributes/default.rb
@@ -27,6 +27,7 @@ override["configure"]["update"] = true
 override["configure"]["upgrade"] = true
 
 default["datadog"]["histogram_aggregates"] = "max, avg"
+default["datadog"]["tracer"]["version"] = "0.72.0"
 
 default["dnsmasq"]["dns"] = {
   "all-servers" => nil,
--- base/recipes/datadog.rb
+++ base/recipes/datadog.rb
@@ -1,17 +1,32 @@
 include_recipe "datadog::dd-agent"
 
-cookbook_file "datadog-setup.php" do
-  path "/usr/local/bin/datadog-setup.php"
+version = node["datadog"]["tracer"]["version"]
+remote_file "datadog-php-tracer" do
+  source "https://github.com/DataDog/dd-trace-php/releases/download/#{version}/datadog-php-tracer_#{version}_amd64.deb"
+  path "/usr/local/bin/datadog-php-tracer-#{version}.deb"
   owner "root"
   group "root"
   mode "0755"
   action :create
-  notifies :run, "execute[datadog-profiler]", :immediately
+end
+
+dpkg_package "datadog-php-tracer" do
+  source "/usr/local/bin/datadog-php-tracer-#{version}.deb"
+  action :install
+end
+
+remote_file "datadog-installer" do
+  source "https://github.com/DataDog/dd-trace-php/releases/download/#{version}/datadog-setup.php"
+  path "/usr/local/bin/datadog-installer-#{version}.php"
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
 end
 
 execute "datadog-profiler" do
-  command "/usr/bin/php '/usr/local/bin/datadog-setup.php' '--enable-profiling' '--php-bin=php' '--php-bin=php#{node["php"]["version"]}' '--php-bin=php-fpm#{node["php"]["version"]}'"
-  action :nothing
+  command "/usr/bin/php '/usr/local/bin/datadog-installer-#{version}.php' '--enable-profiling' '--php-bin=php' '--php-bin=php#{node["php"]["version"]}' '--php-bin=php-fpm#{node["php"]["version"]}'"
+  action :run
 end
 
 edit_resource(:service, "datadog-agent") do
