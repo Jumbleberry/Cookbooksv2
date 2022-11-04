c1466a627b3351a14591edbde1d47e9c198b7c73
ahosking <alexander.hosking@jumbleberry.com>
2022-11-04 15:18:41 -0400

    Correct nvme plugin for new Chef support

--- configure/files/nvme.rb
+++ configure/files/nvme.rb
@@ -1,12 +1,12 @@
 Ohai.plugin(:NVMe) do
   provides "nvme"
   depends "ec2"
-  depends "filesystem2/by_device/mounts"
+  depends "filesystem/by_device/mounts"
 
   collect_data(:default) do
     if ec2
-      filesystem2[:by_device].each do |path, device|
-        if path =~ /nvme[1-9][a-z][1-9]$/ && !filesystem2[:by_device].keys.any? { |d| d != path && d.include?(path) } && !(device[:mounts] || []).any? { |m| m == "/" }
+      filesystem[:by_device].each do |path, device|
+        if path =~ /nvme[1-9][a-z][1-9]$/ && !filesystem[:by_device].keys.any? { |d| d != path && d.include?(path) } && !(device[:mounts] || []).any? { |m| m == "/" }
           return nvme(device.merge!({ :name => File.basename(path), :path => path }))
         end
       end
