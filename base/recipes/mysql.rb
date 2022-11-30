fd0bcd5366956e594fcae8ac3612204790d96fc3
Bellardia <ian@jumbleberry.com>
2022-11-30 13:42:50 -0500

    Fix mysql install for arm

--- base/recipes/mysql.rb
+++ base/recipes/mysql.rb
@@ -25,7 +25,12 @@ if (node["lsb"]["release"].to_i < 18)
     notifies :disable, "service[mysql]", :immediately
   end
 else
-  package_arch = node["kernel"]["machine"] =~ /x86_64/ ? "amd64" : "i386"
+  package_arch = case node['kernel']['machine']
+    when 'x86_64', 'amd64' then 'amd64'
+    when 'aarch64', 'arm64' then 'aarch64'
+    when 'i386' then 'i386'
+    else node['kernel']['machine']
+  end
   package_url = "https://downloads.mysql.com/archives/get/p/23/file/mysql-server_5.6.51-1debian9_#{package_arch}.deb-bundle.tar"
   package_file = "/tmp/mysql-5.6.51.deb"
 
@@ -51,7 +56,7 @@ else
 
   ["mysql-common", "mysql-community-client", "mysql-client", "mysql-community-server", "mysql-server"].each do |p|
     package p do
-      source "/tmp/#{p}_5.6.51-1debian9_amd64.deb"
+      source "/tmp/#{p}_5.6.51-1debian9_#{package_arch}.deb"
       provider Chef::Provider::Package::Dpkg if node["platform_family"] == "debian"
       action :nothing
     end
