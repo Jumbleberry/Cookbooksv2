a5781e5b57190b8b815434695272146f3f0d2254
Bellardia <ian@jumbleberry.com>
2022-11-30 15:15:18 -0500

    Fix timescale installation

--- base/recipes/timescale.rb
+++ base/recipes/timescale.rb
@@ -3,7 +3,7 @@ execute "pgsql-install" do
   command <<-EOH
     sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main' > /etc/apt/sources.list.d/pgdg.list" \
       && wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - \
-      && sudo sh -c "echo 'deb https://packagecloud.io/timescale/timescaledb/ubuntu/ $(lsb_release -cs) main' > /etc/apt/sources.list.d/timescaledb.list" \
+      && sudo sh -c "echo 'deb https://packagecloud.io/timescale/timescaledb/ubuntu/ focal main' > /etc/apt/sources.list.d/timescaledb.list" \
       && wget --quiet -O - https://packagecloud.io/timescale/timescaledb/gpgkey | sudo apt-key add -
   EOH
   user "root"
@@ -12,6 +12,7 @@ end
 # Install timescale
 apt_repository "timescale-ppa" do
   uri "ppa:timescale/timescaledb-ppa2"
+  distribution "focal"
 end
 
 apt_update "update-timescale-2" do
@@ -32,10 +33,7 @@ package "pgloader" do
   action :remove
 end
 
-package "timescaledb-2-postgresql-14" do
-  options '-o Dpkg::Options::="--force-overwrite"'
-  action :install
-end
+execute 'DEBIAN_FRONTEND=noninteractive apt-get install -yq timescaledb-2-loader-postgresql-14 timescaledb-2-postgresql-14 -o Dpkg::Options::="--force-overwrite"'
 
 # define postgresql service
 service "postgresql.service" do
