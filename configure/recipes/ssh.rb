acc5c2e479f5af0e3853cc10c271695269357c12
Bellardia <ian@jumbleberry.com>
2022-12-07 11:53:43 -0500

    Use systemd and delay sshd restart

--- configure/recipes/ssh.rb
+++ configure/recipes/ssh.rb
@@ -3,19 +3,20 @@ unless node.attribute?(:ec2)
     path "/etc/ssh/sshd_config"
     pattern "PermitRootLogin.*"
     line "PermitRootLogin yes"
-    notifies :restart, "service[sshd.service]"
+    notifies :restart, "service[sshd.service]", :delayed
   end
 
   replace_or_add "sshd_config_password_login" do
     path "/etc/ssh/sshd_config"
     pattern "PasswordAuthentication.*"
     line "PasswordAuthentication yes"
-    notifies :restart, "service[sshd.service]"
+    notifies :restart, "service[sshd.service]", :delayed
   end
 
   edit_resource(:service, "sshd.service") do
     service_name "sshd"
     supports status: true, restart: true, reload: true
+    provider Chef::Provider::Service::Systemd
     action node["configure"]["services"]["sshd"] || %i{stop disable}
   end
 end
