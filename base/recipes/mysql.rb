7e701026631380b7a2517d102dddb14670b44f30
ahosking <alexander.hosking@jumbleberry.com>
2022-12-12 15:16:43 -0500

    Update mysql to 8

--- base/recipes/mysql.rb
+++ base/recipes/mysql.rb
@@ -1,18 +1,26 @@
-# Install mysql server
+apt_repository "mysql-ppa" do
+  uri "http://ports.ubuntu.com/ubuntu-ports"
+  distribution "focal"
+  components ["main", "restricted"]
+end
+
+package "mysql-server-8.0" do
+  action :remove
+end
+
 execute "mysql-install" do
   command <<-EOH
-    { \
-      echo "mysql-server-5.7 mysql-server-5.7/mysql_password password '#{node["mysql"]["root_password"]}'"; \
-      echo "mysql-server-5.7 mysql-server-5.7/mysql_password_again password '#{node["mysql"]["root_password"]}'"; \
-      echo "mysql-server-5.7 mysql-server-5.7/data-dir select ''"; \
-      echo "mysql-server-5.7 mysql-server-5.7/root-pass password '#{node["mysql"]["root_password"]}'"; \
-      echo "mysql-server-5.7 mysql-server-5.7/re-root-pass password '#{node["mysql"]["root_password"]}'"; \
-      echo "mysql-server-5.7 mysql-server-5.7/remove-test-db select true"; \
-    } | debconf-set-selections \
-      && DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server-5.7 mysql-server;
-    
-    mkdir -p /var/lib/mysql /var/run/mysqld \
-	    && chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+  { \
+    echo "mysql-server-8.0 mysql-server-8.0/mysql_password password '#{node["mysql"]["root_password"]}'"; \
+    echo "mysql-server-8.0 mysql-server-8.0/mysql_password_again password '#{node["mysql"]["root_password"]}'"; \
+    echo "mysql-server-8.0 mysql-server-8.0/data-dir select ''"; \
+    echo "mysql-server-8.0 mysql-server-8.0/root-pass password '#{node["mysql"]["root_password"]}'"; \
+    echo "mysql-server-8.0 mysql-server-8.0/re-root-pass password '#{node["mysql"]["root_password"]}'"; \
+    echo "mysql-server-8.0 mysql-server-8.0/remove-test-db select true"; \
+  } | debconf-set-selections \
+  && DEBIAN_FRONTEND=noninteractive apt-get install -yq apt-utils mysql-server-8.0 mysql-server;
+  mkdir -p /var/lib/mysql /var/run/mysqld \
+  && chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
   EOH
   ignore_failure true
   notifies :edit, "replace_or_add[mysql-dpkg-configure]", :immediately
@@ -41,3 +49,10 @@ service "mysql" do
   provider Chef::Provider::Service::Systemd
   action :nothing
 end
+
+# apt_repository "mysql-ppa" do
+#   uri "http://ports.ubuntu.com/ubuntu-ports"
+#   distribution "focal"
+#   components ["main", "restricted"]
+#   action :remove
+# end
