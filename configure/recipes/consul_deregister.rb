20b352d61b308cade72b28d343c21a276aff914f
Bellardia <ian@jumbleberry.com>
2022-12-12 10:20:04 -0500

    Stop consul after leaving

--- configure/recipes/consul_deregister.rb
+++ configure/recipes/consul_deregister.rb
@@ -1,3 +1,10 @@
 execute "consul leave" do
   notifies :stop, "service[consul.service]", :delayed
 end
+
+edit_resource(:service, "consul.service") do
+  service_name "consul"
+  supports status: true, restart: true, reload: true
+  provider Chef::Provider::Service::Systemd
+  action :nothing
+end
