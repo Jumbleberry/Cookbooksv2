429f225cdec67d90e023b4cf7b6165f0245cb91b
CoffeeOrDeath <alexander.hosking@jumbleberry.com>
2023-04-06 15:45:42 -0400

    ci: Add Secondary Sleuth Deployment Marker (#82)

--- jbx/recipes/deployment_markers.rb
+++ jbx/recipes/deployment_markers.rb
@@ -21,11 +21,22 @@ ruby_block "sleuth_deployment" do
         use_ssl: uri.scheme == "https",
         }
 
+        # Add a Second Call to track changes in two deployment patterns within Sleuth
+        uri = URI.parse("https://app.sleuth.io/api/1/deployments/galactic-propeller/jbx-2/register_deploy")
+        request = Net::HTTP::Post.new(uri)
+        request.set_form_data(
+        "api_key" => "#{data_hash['sleuth']['key']}",
+        "environment" => "#{data_hash['sleuth']['env']}",
+        "sha" => "#{commit_hash.strip}",
+        )
+
+        req_options = {
+        use_ssl: uri.scheme == "https",
+        }
+
         response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
         http.request(request)
         end
-        Chef::Log.warn("#{response.code}")
-        Chef::Log.warn("#{response.body}")
     end
     action :run
 end
