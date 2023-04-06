c0a705c741106bb0c6d2fc35867d32c2c27bf011
CoffeeOrDeath <alexander.hosking@jumbleberry.com>
2023-04-06 16:29:39 -0400

    fix: deployment marker code is wrong (#83)
    * ci: Add Secondary Sleuth Deployment Marker
    
    * fix: Correct Deployment Marker Calls

--- jbx/recipes/deployment_markers.rb
+++ jbx/recipes/deployment_markers.rb
@@ -20,6 +20,22 @@ ruby_block "sleuth_deployment" do
         req_options = {
         use_ssl: uri.scheme == "https",
         }
+        response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+            http.request(request)
+        end
+
+        # Add a Second Call to track changes in two deployment patterns within Sleuth
+        uri = URI.parse("https://app.sleuth.io/api/1/deployments/galactic-propeller/jbx-2/register_deploy")
+        request = Net::HTTP::Post.new(uri)
+        request.set_form_data(
+        "api_key" => "#{data_hash['sleuth']['key']}",
+        "environment" => "#{data_hash['sleuth']['env']}",
+        "sha" => "#{commit_hash.strip}",
+        )
+
+        req_options = {
+        use_ssl: uri.scheme == "https",
+        }
 
         # Add a Second Call to track changes in two deployment patterns within Sleuth
         uri = URI.parse("https://app.sleuth.io/api/1/deployments/galactic-propeller/jbx-2/register_deploy")
@@ -35,7 +51,7 @@ ruby_block "sleuth_deployment" do
         }
 
         response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-        http.request(request)
+            http.request(request)
         end
     end
     action :run
