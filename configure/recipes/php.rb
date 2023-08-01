74f9983be89e0aa47cd6716357cc18f89583f051
Bellardia <ian@jumbleberry.com>
2023-08-01 16:58:45 -0400

    Tune settings for PHP 8 (#86)

--- configure/recipes/php.rb
+++ configure/recipes/php.rb
@@ -1,10 +1,21 @@
+template "/etc/php/#{node["php"]["version"]}/mods-available/opcache.ini" do
+  manage_symlink_source true
+  source "opcache.ini.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :reload, "service[php#{node["php"]["version"]}-fpm.service]", :delayed
+end
+
 # Fpm configurations
 node["php"]["fpm"]["conf_dirs"].each do |path|
   template path + "/php.ini" do
     source "php.ini.erb"
     variables({
+      "sapi" => ::File.basename(path),
       "display_errors" => node["php"]["fpm"]["display_errors"],
       "include_path" => node["php"]["fpm"]["include_path"],
+      "preload" => "/var/www/jbx/application/library/Bootstrap.php",
     })
     if path.include? "fpm"
       notifies :reload, "service[php#{node["php"]["version"]}-fpm.service]", :delayed
@@ -34,7 +45,7 @@ node["php"]["fpm"]["conf_dirs"].each do |path|
   end
 end
 
-include_recipe 'logrotate'
+include_recipe "logrotate"
 
 logrotate_app "php" do
   path node["php"]["logfile"]
